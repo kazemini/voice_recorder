c443435c8644ffff1d8ffcb7af19b0e05ce6cdae
Ali Kazemini <kazemini.business@gmail.com>
2023-07-20 23:04:04 +0330

    merge recorder with jsu ui

--- lib/core/widgets/main_wrapper.dart
+++ lib/core/widgets/main_wrapper.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:simple_ripple_animation/simple_ripple_animation.dart';
 import 'package:template/config/theme/theme_cubit.dart';
 
 import '../../config/utils/enums_config.dart';
@@ -22,13 +23,26 @@ class MainWrapper extends StatefulWidget {
   State<MainWrapper> createState() => _MainWrapperState();
 }
 
-class _MainWrapperState extends State<MainWrapper> {
+class _MainWrapperState extends State<MainWrapper> with TickerProviderStateMixin {
   final recorder = FlutterSoundRecorder();
   late final String _path;
+  late final AnimationController _controller;
+  late final CurvedAnimation _animation;
+  bool _isRecording = false;
+
   @override
   void initState() {
     super.initState();
     initRecorder();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(seconds: 1), lowerBound: 0.5, upperBound: 1)
+      ..forward()
+      ..addStatusListener((status) {
+        if (_controller.isCompleted) {
+          _controller.repeat(reverse: true);
+        }
+      });
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
   }
 
   @override
@@ -57,9 +71,9 @@ class _MainWrapperState extends State<MainWrapper> {
   }
 
   Future stop() async {
-   final path = await recorder.stopRecorder();
-   final audioFile = File(path!);
-   print('Recorded path is : $path');
+    final path = await recorder.stopRecorder();
+    final audioFile = File(path!);
+    print('Recorded path is : $path');
   }
 
   @override
@@ -120,53 +134,290 @@ class _MainWrapperState extends State<MainWrapper> {
       ),
       bottomNavigationBar: Container(
         width: MediaQuery.of(context).size.width,
-        height: 60,
-        margin: const EdgeInsets.only(left: 8.0,right: 8.0,bottom: 8.0),
-
-        child: Row(
+        height: 62,
+        margin: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+        child: Stack(
           children: [
-            Expanded(
+         _buildBottomSection(),
+            /*
+               Expanded(
               child: Container(
-                height: 45,
+                alignment: Alignment.center,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(32),
-                    color: Colors.green
-                ),
+                    borderRadius: BorderRadius.circular(32), color: Colors.green),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
                   children: [
-                    StreamBuilder<RecordingDisposition>(
-                        stream: recorder.onProgress,
-                        builder: (context, snapshot) {
-                          final duration = snapshot.hasData ? snapshot.data!.duration : Duration.zero;
-                          String twoDigits(int n) => n.toString().padLeft(2, '0');
-                          final twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-                          final twoDigitsSeconds = twoDigits(duration.inSeconds.remainder(60));
+                    Container(
+                      margin: EdgeInsets.only(left: 8.0, right: 8.0),
+                      alignment: Alignment.centerRight,
+                      child: StreamBuilder<RecordingDisposition>(
+                          stream: recorder.onProgress,
+                          builder: (context, snapshot) {
+                            final duration =
+                            snapshot.hasData ? snapshot.data!.duration : Duration.zero;
+                            String twoDigits(int n) => n.toString().padLeft(2, '0');
+                            final twoDigitMinutes =
+                            twoDigits(duration.inMinutes.remainder(60));
+                            final twoDigitsSeconds =
+                            twoDigits(duration.inSeconds.remainder(60));
 
-                          return Text(
-                            '$twoDigitMinutes:$twoDigitsSeconds',
-                            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold,color: Colors.white),
-                          );
-                        }),
+                            return Text(
+                              '$twoDigitMinutes:$twoDigitsSeconds',
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            );
+                          }),
+                    ),
+                    recorder.isRecording ? Text('برای لغو به راست بکشید') : SizedBox()
                   ],
                 ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.all(6.0),
-              child: ElevatedButton(
-                onPressed: () async {
-                  recorder.isRecording ? await stop() : await record();
-                  setState(() {});
-                },
-                child: Icon(recorder.isRecording ? Icons.pause : Icons.mic, size: 35),
-              ),
-            )
+            */
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildRecordSection() {
+    if (_isRecording) {
+      record();
+    }
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0, top: 4.0),
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 3)],
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 8.0),
+                      child: Listener(
+                          onPointerUp: (details) async {
+                            await stop();
+                            _isRecording = false;
+                            setState(() {});
+                          },
+                          child: const RippleAnimation(
+                            color: Colors.cyanAccent,
+                            delay: Duration(milliseconds: 200),
+                            repeat: true,
+                            minRadius: 20,
+                            ripplesCount: 3,
+                            duration: Duration(milliseconds: 6 * 300),
+                            child: Icon(
+                              Icons.mic,
+                              color: Colors.blue,
+                              size: 30,
+                            ),
+                          )),
+                    ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: StreamBuilder<RecordingDisposition>(
+                            stream: recorder.onProgress,
+                            builder: (context, snapshot) {
+                              final duration =
+                                  snapshot.hasData ? snapshot.data!.duration : Duration.zero;
+                              String twoDigits(int n) => n.toString().padLeft(2, '0');
+                              final twoDigitMinutes =
+                                  twoDigits(duration.inMinutes.remainder(60));
+                              final twoDigitsSeconds =
+                                  twoDigits(duration.inSeconds.remainder(60));
+                              return Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 12.0),
+                                    child: Text(
+                                      '$twoDigitMinutes:$twoDigitsSeconds',
+                                      style: const TextStyle(
+                                          fontSize: 14, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  FadeTransition(
+                                    opacity: _animation,
+                                    child: Text(
+                                      'درحال ضبط صدا...',
+                                      textDirection: TextDirection.rtl,
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black87),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottomSection() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0, top: 4.0),
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 3)],
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(left: 8.0),
+                      child: Listener(
+                        onPointerDown: (details) async {
+                          await record();
+                          setState(() {
+                            _isRecording = true;
+                          });
+                        },
+                        onPointerUp: (details) async {
+                          await stop();
+                          setState(() {
+                            _isRecording = false;
+                          });
+                        },
+                        child: recorder.isRecording
+                            ? const RippleAnimation(
+                                color: Colors.cyanAccent,
+                                delay: Duration(milliseconds: 200),
+                                repeat: true,
+                                minRadius: 20,
+                                ripplesCount: 3,
+                                duration: Duration(milliseconds: 6 * 300),
+                                child: Icon(
+                                  Icons.mic,
+                                  color: Colors.blue,
+                                  size: 30,
+                                ),
+                              )
+                            : const Icon(
+                                Icons.mic,
+                                color: Colors.blueGrey,
+                                size: 30,
+                              ),
+                      ),
+                    ),
+                    Visibility(
+                      visible: !_isRecording,
+                      replacement: Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: StreamBuilder<RecordingDisposition>(
+                              stream: recorder.onProgress,
+                              builder: (context, snapshot) {
+                                final duration =
+                                    snapshot.hasData ? snapshot.data!.duration : Duration.zero;
+                                String twoDigits(int n) => n.toString().padLeft(2, '0');
+                                final twoDigitMinutes =
+                                    twoDigits(duration.inMinutes.remainder(60));
+                                final twoDigitsSeconds =
+                                    twoDigits(duration.inSeconds.remainder(60));
+                                return Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 16.0),
+                                      child: Text(
+                                        '$twoDigitMinutes:$twoDigitsSeconds',
+                                        style: const TextStyle(
+                                            fontSize: 14, fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    FadeTransition(
+                                      opacity: _animation,
+                                      child: Text(
+                                        'درحال ضبط صدا...',
+                                        textDirection: TextDirection.rtl,
+                                        style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black87),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }),
+                        ),
+                      ),
+                      child: const Expanded(
+                        child: TextField(
+                          style: TextStyle(fontFamily: "vazir"),
+                          textAlign: TextAlign.right,
+                          textDirection: TextDirection.rtl,
+                          keyboardType: TextInputType.multiline,
+                          minLines: 1,
+                          maxLines: 7,
+                          decoration:
+                              InputDecoration(border: InputBorder.none, hintText: "متن پیام"),
+                          autofocus: false,
+                        ),
+                      ),
+                    ),
+                    !_isRecording ? IconButton(
+                      tooltip: 'انتخاب فایل',
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.attach_file,
+                        color: Colors.blueGrey,
+                      ),
+                    ) : const SizedBox()
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        !_isRecording ? Container(
+          margin: EdgeInsets.only(bottom: 8.0, right: 8.0),
+          padding: EdgeInsets.zero,
+          decoration: ShapeDecoration(color: Colors.blue, shape: CircleBorder()),
+          child: IconButton(
+            icon: Icon(Icons.send_rounded, color: Colors.white),
+            onPressed: () {},
+          ),
+        ) : SizedBox()
+      ],
     );
   }
 }
